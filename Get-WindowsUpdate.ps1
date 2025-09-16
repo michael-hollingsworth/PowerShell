3644c995f4378d0840709caeca198790e0ea02c9
michael-hollingsworth <michaelhollingsworth1093@gmail.com>
2025-09-16 08:43:18 -0400

    Update Get-WindowsUpdate.ps1

--- Get-WindowsUpdate.ps1
+++ Get-WindowsUpdate.ps1
@@ -14,7 +14,7 @@ function Install-WindowsUpdate {
 
         [__ComObject]$searchResults = $searcher.Search($query)
 
-        Write-Verbose -Message "[$($searchResults.Updates.Count)] updates are available."
+        $PSCmdlet.WriteVerbose("[$($searchResults.Updates.Count)] updates are available.")
 
         if ($searchResults.Updates.Count -lt 1) {
             return
@@ -24,10 +24,10 @@ function Install-WindowsUpdate {
         foreach ($update in $searchResults.Updates) {
             if ((-not $update.EulaAccepted) {
                 if (-not $AcceptEula) {
-                    Write-Warning -Message "The update [$($update.Title)] requires the EULA to be accpeted to install it. To accept the EULA use the [-AcceptEula] parameter.
+                    $PSCmdlet.WriteWarning("The update [$($update.Title)] requires the EULA to be accpeted to install it. To accept the EULA use the [-AcceptEula] parameter.")
                     continue
                 }
-                Write-Verbose -Message "Accepting the EULA for the update [$($update.Title)] with the following terms and conditions: rn$($update.EulaText)"
+                $PSCmdlet.WriteWarning("Accepting the EULA for the update [$($update.Title)] with the following terms and conditions: rn$($update.EulaText)")
                 $update.AcceptEula()
             }
             $null = $updsteColl.Add($update)
@@ -40,12 +40,12 @@ function Install-WindowsUpdate {
 
         [__ComObject]$downloader = $session.CreateUpdateDownloader()
         $downloader.Updates = $updateColl.Updates
-        Write-Verbose -Message "Downloading [$($updateColl.Updates.Count)] updates."
+        $PSCmdlet.WriteVerbose("Downloading [$($updateColl.Updates.Count)] updates.")
         [__ComObject]$downloadResults = $downloader.Download()
 
         [__ComObject]$installer = $session.CreateUpdateInstaller()
         $installer.Updates = $updateColl.Updates
-        Write-Verbose -Message "Installing [$($updateColl.Updates.Count)] updates."
+        $PSCmdlet.WriteVerbose("Installing [$($updateColl.Updates.Count)] updates.")
         [__ComObject]$installResults = $installer.Install()
     } end {
         if ($session -is [__ComObject]) {
