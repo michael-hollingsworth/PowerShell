6740ccfbb0477f572aaad962fd54ce9403c4774a
michael-hollingsworth <michaelhollingsworth1093@gmail.com>
2025-10-30 21:18:06 -0400

    update

--- Get-ADComputerLastLogon.ps1
+++ Get-ADComputerLastLogon.ps1
@@ -5,7 +5,8 @@ function Get-ADComputerLastLogon {
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0, ParameterSetName = 'Identity')]
         #TODO: figure out what kinda black magic Microsoft uses to get the Identity parameter to work
         ## While you're at it, see if it is possible to do all of this inside the native ADComputer object
-        [String[]]$Identity,
+        [ValidateNotNullOrEmpty()]
+        [Microsoft.ActiveDirectory.Management.ADComputer[]]$Identity,
         [Parameter(Mandatory = $true, ParameterSetName = 'Filter')]
         [ValidateNotNullOrEmpty()]
         [String]$Filter,
@@ -19,45 +20,34 @@ function Get-ADComputerLastLogon {
     )
 
     begin {
-        [System.Text.RegularExpressions.Regex]$dnPattern = [Regex]::new('(?:(?<attr>[A-Za-z][A-Za-z0-9-]*)=(?<value>(?:\\.|[^,\\])+))(?:,|$)')
         #TODO: Add the remaining default properties
         #TODO: See if the modifications can be made to the original class without modifying the original object to the point that it breaks things when passing it to Set-ADComputer
-        [String[]]$searchProperties = @('LastLogon', 'LastLogonTimestamp')
+        [String[]]$gadcProperties = @('LastLogon', 'LastLogonTimestamp')
         $soProperties = @(
             'Name',
             'Enabled',
-            @{ Name = 'LastLogon'; Expression = { [DateTime]::FromFileTime($_.LastLogon) } },
-            @{ Name = 'LastLogonTimestamp'; Expression = { [DateTime]::FromFileTime($_.LastLogonTimestamp) } },
-            @{ Name = 'OUPath'; Expression = {
-                [System.Text.RegularExpressions.MatchCollection]$regexMatches = $dnPattern.Matches($_.DistinguishedName)
-                [String[]]$components = foreach ($group in $regexMatches) {
-                    if ($group.Groups[0].Value.StartsWith('CN=')) {
-                        continue
-                    }
-
-                    if ($group.Groups[0].Value.StartsWith('DC=')) {
-                        continue
-                    }
-
-                    $group.Groups.Where({$_.Name -eq 'value'}).Value
-                }
-
-                [Array]::Reverse($components)
-                $components -join '\'
-            }},
+            @{
+                Name = 'LastLogon'
+                Expression = { [DateTime]::FromFileTime($_.LastLogon) }
+            },
+            @{
+                Name = 'LastLogonTimestamp'
+                Expression = { [DateTime]::FromFileTime($_.LastLogonTimestamp) }
+            },
             'DistinguishedName',
             'Sid',
             'GUID'
         )
 
         if ($Properties) {
-            $searchProperties += $Properties
+            $gadcProperties += $Properties
             $soProperties += $Properties
         }
     } process {
         [Microsoft.ActiveDirectory.Management.ADComputer[]]$computers = if ($PSCmdlet.ParameterSetName -eq 'Filter') {
             [HashTable]$gadcSplat = @{
                 Filter = $Filter
+                Properties = $gadcProperties
                 ErrorAction = [System.Management.Automation.ActionPreference]::Stop
             }
 
@@ -68,7 +58,7 @@ function Get-ADComputerLastLogon {
             Get-ADComputer @gadcSplat
         } else {
             foreach ($id in $Identity) {
-                Get-ADComputer -Identity $id -Properties $searchProperties -ErrorAction Continue
+                Get-ADComputer -Identity $id -Properties $gadcProperties -ErrorAction Continue
             }
         }
 
--- New-ADComputerReport.ps1
+++ New-ADComputerReport.ps1
@@ -3,7 +3,7 @@ function New-ADComputerReport {
     param (
         #TODO: Add the rest of the parameters to reach feature parody with Get-ADComputer
         [Parameter(Mandatory = $true, ValueFromPipeline = $true, Position = 0, ParameterSetName = 'Identity')]
-        [Management[]]$Identity,
+        [Microsoft.ActiveDirectory.Management.ADComputer[]]$Identity,
         [Parameter(Mandatory = $true, ParameterSetName = 'Filter')]
         [ValidateNotNullOrEmpty()]
         [String]$Filter,
@@ -17,23 +17,73 @@ function New-ADComputerReport {
     )
 
     begin {
-        $baseProperties = @()
+        [System.Text.RegularExpressions.Regex]$dnPattern = [Regex]::new('(?:(?<attr>[A-Za-z][A-Za-z0-9-]*)=(?<value>(?:\\.|[^,\\])+))(?:,|$)')
+        #TODO: Add the remaining default properties
+        #TODO: See if the modifications can be made to the original class without modifying the original object to the point that it breaks things when passing it to Set-ADComputer
+        [String[]]$gadcProperties = @('LastLogon', 'LastLogonTimestamp', 'Description', 'Info')
+        $soProperties = @(
+            'Name',
+            'Enabled',
+            @{
+                Name = 'LastLogon'
+                Expression = { [DateTime]::FromFileTime($_.LastLogon) }
+            },
+            @{
+                Name = 'LastLogonTimestamp'
+                Expression = { [DateTime]::FromFileTime($_.LastLogonTimestamp) }
+            },
+            @{
+                Name = 'OUPath'
+                Expression = {
+                    [System.Text.RegularExpressions.MatchCollection]$regexMatches = $dnPattern.Matches($_.DistinguishedName)
+                    [String[]]$components = foreach ($group in $regexMatches) {
+                        if ($group.Groups[0].Value.StartsWith('CN=')) {
+                            continue
+                        }
+
+                        if ($group.Groups[0].Value.StartsWith('DC=')) {
+                            continue
+                        }
+
+                        $group.Groups.Where({$_.Name -eq 'value'}).Value
+                    }
+
+                    [Array]::Reverse($components)
+                    $components -join '\'
+                }
+            },
+            'Description',
+            'Info',
+            'DistinguishedName',
+            'Sid',
+            'GUID'
+        )
+
+        if ($Properties) {
+            $gadcProperties += $Properties
+            $soProperties += $Properties
+        }
     } process {
         [Microsoft.ActiveDirectory.Management.ADComputer[]]$computers = if ($PSCmdlet.ParameterSetName -eq 'Filter') {
             [HashTable]$gadcSplat = @{
                 Filter = $Filter
+                Properties = $gadcProperties
                 ErrorAction = [System.Management.Automation.ActionPreference]::Stop
             }
-    
+
             if (-not [String]::IsNullOrWhiteSpace($SearchBase)) {
                 $gadcSplat.Add('SearchBase', $SearchBase)
             }
-    
+
             Get-ADComputer @gadcSplat
         } else {
             foreach ($id in $Identity) {
-                Get-ADComputer -Identity $id -Properties $searchProperties -ErrorAction Continue
+                Get-ADComputer -Identity $id -Properties $gadcProperties -ErrorAction Continue
             }
+        }
+
+        foreach ($comp in $computers) {
+            $PSCmdlet.WriteObject((Select-Object -InputObject $comp -Property $soProperties -ErrorAction Continue))
         }
     }
 }
